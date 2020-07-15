1637047991709e2e67ed41f9ced13d691ea9584a
Brodie Curtis <curtis.brodie1011@yahoo.com>
2020-07-15 01:02:57 +0000

    Automatically backed up by Learn

--- lib/post.rb
+++ lib/post.rb
@@ -1,9 +1,19 @@
 class Post
   
-  attr_accessor :name
+  attr_accessor :name, :author
   @@all = []
   
   def initialize(name)
     @name = name
+    @@all << self
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def add_author(author)
+    self.author = author
+    author.post << self
   end
 end
